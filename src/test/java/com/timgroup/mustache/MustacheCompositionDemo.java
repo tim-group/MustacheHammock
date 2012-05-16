e455600d6f7bea3b27ea72dcb8fc3bce501bfb80
Tom Anderson <tom.anderson@timgroup.com>
2012-05-16 12:11:57 +0100

    Added initial Mustache demo program

---
+++ src/test/java/com/timgroup/mustache/MustacheCompositionDemo.java
@@ -0,0 +1,22 @@
+package com.timgroup.mustache;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+public class MustacheCompositionDemo {
+    
+    public static void main(String[] args) throws IOException {
+        render("hello.mustache", new PrintWriter(System.out));
+    }
+
+    private static void render(String template, PrintWriter output) throws IOException {
+        MustacheFactory factory = new DefaultMustacheFactory();
+        Mustache mustache = factory.compile(template);
+        mustache.execute(output, new Object[] {}).flush();
+    }
+    
+}
